29e5480b5c11254cfd5701080734775ba518fdbd
Nuclear One <ajithsnair10@gmail.com>
2022-04-18 17:14:38 +0530

    Mail send function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Route, Switch } from "react-router-dom";
 import Footer from "./Component/Footer/Footer";
 import NavBar from "./Component/NavBar/NavBar";
 import LoginPage from "./Component/Pages/LoginPage/LoginPage";
+import Compose from "./Component/Pages/MailBox/Compose";
 import Welcome from "./Component/Pages/WelcomePage/Welcome";
+
 
 function App() {
   return (
     <Fragment>
       <NavBar />
+      
       <Switch>
         <Route path="/auth" >
           <LoginPage />
@@ -16,8 +19,12 @@ function App() {
         <Route path="/welcome">
           <Welcome />
         </Route>
+        <Route path="/mailbox">
+          <Compose />
+        </Route>
       </Switch>
       <Footer />
+      
     </Fragment>
   );
 }
