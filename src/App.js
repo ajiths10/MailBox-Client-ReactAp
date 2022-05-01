5d0e63cb845108f36fd6b51dd95e25590b1d9629
Nuclear One <ajithsnair10@gmail.com>
2022-05-01 13:08:37 +0530

    Authentication access page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import { Fragment } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Footer from "./Component/Footer/Footer";
 import NavBar from "./Component/NavBar/NavBar";
 import LoginPage from "./Component/Pages/LoginPage/LoginPage";
 import MailBoxBody from "./Component/Pages/MailBox/Body/MailBoxBody";
 import Loading from './Component/Loading/Loading';
 import Welcome from "./Component/Pages/WelcomePage/Welcome";
+import { useSelector } from "react-redux";
 
 
 function App() {
+  const isLogin = useSelector(state=>state.auth.isAuth);
   return (
     <Fragment>
       <NavBar />
       
       <Switch>
         <Route path="/auth" >
-          <LoginPage />
+          { !isLogin &&<LoginPage />}
         </Route>
-        <Route path="/welcome">
-          <Welcome />
+        <Route path="/welcome"> 
+          { isLogin && <Welcome />}
+          { !isLogin &&  <Redirect to='/auth'></Redirect>   }
         </Route>
         <Route path="/mailbox">
-          <MailBoxBody />
+        { isLogin &&  <MailBoxBody /> }
+        { !isLogin &&  <Redirect to='/auth'></Redirect>  }
         </Route>
         <Route path="/about">
           <Loading />
